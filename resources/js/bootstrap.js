337e1728fe0d289d278f4ebcc5fd8dd2eea80f60
Brendon Baugh <brendonbaughray@gmail.com>
2026-01-30 11:15:30 -0600

    Fixed logout

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,16 @@
 import axios from 'axios';
-window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] =
+    'XMLHttpRequest';
+
+if (typeof document !== 'undefined') {
+    const tokenElement = document.head.querySelector(
+        'meta[name="csrf-token"]',
+    );
+
+    if (tokenElement) {
+        window.axios.defaults.headers.common['X-CSRF-TOKEN'] =
+            tokenElement.content;
+    }
+}
