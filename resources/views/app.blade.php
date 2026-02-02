4de851817ac57c805da5378fdf0c2729f4d09dcb
Brendon Baugh <brendonbaughray@gmail.com>
2026-02-02 12:00:29 -0600

    Added favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,10 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+    <link rel="icon" href="/images/favicon.ico">
     <link rel="manifest" href="/manifest.webmanifest">
     <meta name="theme-color" content="#f8fafc">
 
